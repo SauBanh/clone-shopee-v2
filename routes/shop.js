51a5bfe4d012cf7fd8d4c4576e013cf57faad21f
SauBanh <saubanh05062001@gmail.com>
2023-04-25 00:08:07 +0700

    add fix layout add route cart, prodcut detail and setup page cart and product detail

--- routes/shop.js
+++ routes/shop.js
@@ -3,7 +3,15 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-    res.render("layouts/main-layout");
+    res.render("shop/home");
+});
+
+router.get("/cart", (req, res, next) => {
+    res.render("shop/cart");
+});
+
+router.get("/product", (req, res, next) => {
+    res.render("shop/product-detail");
 });
 
 module.exports = router;
